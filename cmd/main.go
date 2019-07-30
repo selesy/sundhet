9c2ab56ab759cea19c6037fdc233fb55f4bba49a
Steve Moyer <smoyer@psu.edu>
2019-07-30 16:05:59 -0400

    Application stub (no options yet)

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/kelseyhightower/envconfig"
+	"github.com/selesy/sundhet/pkg/sundhet"
+	log "github.com/sirupsen/logrus"
+)
+
+const kubernetes = `kubernetes`
+
+type Cfg struct {
+}
+
+func main() {
+	var cfg Cfg
+	err := envconfig.Process(kubernetes, &cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	sundhet.Something()
+}
